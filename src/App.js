b0796c1786a7f6f5eab9d84660f6353b166b9e29
Jon Seidel <jseidel@edpci.com>
2024-01-08 07:08:33 -0800

    fix header, footer, perspective

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+const Div = () => {
+  return (
+    <div style={{position: "fixed", top: "60%", height: "40%", width: "100%", maxWidth: "100%", backgroundColor: "hsl(195deg, 20%, 86%)"}}></div>
+  )
+}
 function App() {
   return (
     <>
+      <Div />
       <CharacterEditor />
       <Footer />
     </>
